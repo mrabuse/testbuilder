546da5a4317f643e1fa1a98606bcf83d0a48352f
Megan Rabuse <cogglemat@gmail.com>
2016-12-28 21:50:02 -0600

    Alter first two tests to pass
    Delete the test that only throws an error. Alter "even" test to evaluate to true.

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -16,9 +16,6 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // Once you've read and understood this section, please comment it out. 
   // You will not be able to proceed with a failing test. 
 
-  it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
-  });
 
   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
     // This test doesn't really test anything at all! It will pass no matter what.
@@ -32,7 +29,7 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // A test should only fail if the expected behavior doesn't match the actual.
   it('Throws an error when expected behavior does not match actual behavior', function() {
     var even = function(num){
-      return num/2 === 0;
+      return num%2 === 0;
     }
 
     if(even(10) !== true) {
@@ -46,7 +43,6 @@ describe('Diner\'s Club', function() {
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
-    throw new Error('Delete me!');
   
     if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
