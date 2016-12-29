d0d58ebee33a1d155d1bdda54fb17d4f843a4735
Megan Rabuse <cogglemat@gmail.com>
2016-12-28 21:29:30 -0600

    Add recognition of Visa and Mastercard to detectNetwork

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,10 +13,25 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
   if (cardNumber.length === 14 && (cardNumber.substring(0,2) === "38" || cardNumber.substring(0,2) === "39")) {
   	return "Diner\'s Club";
   } else if (cardNumber.length === 15 && (cardNumber.substring(0,2) === "34" || cardNumber.substring(0,2) === "37")) {
   	return "American Express";
+  } else if (cardNumber.length === 16) {
+  	if (cardNumber.substring(0,1) === "4") {
+  		return "Visa";
+  	} else if (cardNumber.substring(0,1) === "5") {
+  		if (cardNumber.substring(0,1) === "1" || cardNumber.substring(0,1) === "2" || cardNumber.substring(0,1) === "3" || cardNumber.substring(0,1) === "4" || cardNumber.substring(0,1) === "5") {
+  			return "MasterCard";
+  		} else {
+  			return "Credit card network not supported";
+  		}
+  	} else {
+  		return "Credit card network not supported";
+  	}
+  } else if ((cardNumber.length === 13 || cardNumber.length === 19) && cardNumber.substring(0,1) === "4") {
+  	return "Visa";
   }
   else {
   	return "Credit card network not supported";
